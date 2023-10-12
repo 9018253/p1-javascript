45bf3bb88471cd8651dd1a5df97f072e68d3440e
9018253 <144021671+9018253@users.noreply.github.com>
2023-10-12 11:40:53 +0200

    Add files via upload

---
+++ opdracht 6.1/code.js
@@ -0,0 +1,9 @@
+function changecollor() 
+{
+    document.getElementById("kleur").style.color = "green";
+}
+
+function changecollor2() 
+{
+    document.getElementById("kleur2").style.color = "green";
+} 
